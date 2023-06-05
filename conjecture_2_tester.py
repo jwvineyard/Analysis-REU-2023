1faa6ca54620f24acec8b64ae777f09ce8d61cb7
Jimmy Vineyard <72844296+jwvineyard@users.noreply.github.com>
2023-06-05 10:09:24 -0500

    Code should be functional. To use it, try either making a jupyter notebook or using the console.

--- conjecture_2_tester.py
+++ conjecture_2_tester.py
@@ -4,12 +4,14 @@ from numpy.polynomial import polynomial as poly
 
 # generate array of k polynomials of degree n
 # (most definitely not optimized)
-def gen_poly(n, k):
+def gen_polyspace(n, k):
+    # Generate random real and imaginary coefficients in the box from -1 to 1 and sum them
     re_coeff = np.random.uniform(-1,1,(k,n))
     im_coeff = np.random.uniform(-1,1,(k,n))*1j
     coeff =  re_coeff + im_coeff
 
-    poly_arr = np.zeros(k)
+    # Turns coefficient arrays to polynomial array
+    poly_arr = np.empty(k,dtype=poly.Polynomial)
     i = 0
     while(i<k):
         poly_arr[i] = poly.Polynomial(coeff[i])
@@ -21,17 +23,36 @@ def gen_poly(n, k):
 # F is set of points
 # p is polynomial
 def norm_samp(p,F):
+    # Evaluate the polynomial at the points in F
+    values = poly.polyval(F, p.coef)
+
+    # Compute the magnitudes of these values
+    magnitudes = np.abs(values)
+
+    # Return the maximum magnitude
+    return np.max(magnitudes)
+
+
+# p is polynomial, k is number of points to evaluate p at.
+def norm_sup(p,k):
+    theta = np.linspace(0, 2 * np.pi, k)
+    z = np.exp(1j * theta)  # 1j represents the imaginary unit in Python.
+
+    # Evaluate the polynomial at these points
+    values = poly.polyval(z, p.coef)
+
+    # Compute the magnitudes of these values
+    magnitudes = np.abs(values)
+
+    # Return the maximum magnitude
+    return np.max(magnitudes)
+
+def K(n, F, pspace_size = 1,sup_eval_size = 10):
+    polyspace = gen_polyspace(n,pspace_size)
+
+    # Find suprememum of sup/samp given polynomial space P
     sup = 0
-    for f in F:
-        abs = abs(p.__call__(f))
-        if abs > sup: sup = abs
+    for p in polyspace:
+        a = norm_sup(p,sup_eval_size)/norm_samp(p,F)
+        if a > sup: sup = a
     return sup
-
-
-# TODO: write method to find sup norm of polynomial on unit circle
-
-
-# TODO: write function to calculate K(n,F)/K(n,N)
-def K(n,F):
-    #dies
-    return None
